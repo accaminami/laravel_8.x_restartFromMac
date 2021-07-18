d5f3b1853b70e15f0008a2123e587b1c8229462a
南健二 <minamikenji@minamikenjinoMacBook-Pro.local>
2021-07-18 17:52:03 +0900

    Laravel Webアプリケーション開発 第２章

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,45 @@
 
 namespace App\Providers;
 
+use App\BlowFishEncrypter;
+use Illuminate\Encryption\MissingAppKeyException;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
-        //
+        $this->app->singleton(
+            'encrypter',
+            function ($app) {
+                $config = $app->make('config')->get('app');
+
+                return new BlowFishEncrypter($this->parseKey($config));
+            }
+        );
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
+    protected function parseKey(array $config)
+    {
+        if (Str::startsWith($key = $this->key($config), $prefix = 'base64:')){
+            $key = base64_decode(Str::after($key, $prefix));
+        }
+        return $key;
+    }
+
+    protected function key(array $config)
+    {
+        return tap(
+            $config['key'],
+            function ($key){
+                if(empty($key)){
+                    throw new MissingAppKeyException;
+                }
+            }
+        );
+    }
     public function boot()
     {
-        //
     }
 }
